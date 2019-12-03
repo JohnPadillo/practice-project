c9b81df3c60ef6c9791c4aec5f1da5c0c7f036f4
John Rafael Padillo <johnrafaelpadillo@gmail.com>
2019-12-03 17:03:00 +0800

    Fixed Post Method for /register

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,32 +1,47 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    // let { name, email , password } = req.body;
-        let name = req.body.name;
-        User.create({
-            name,
-            email,
-            password
-        })
-    //     .then(user => {
-    //         res.send(`${user}`)
-    //         console.log('Registered Successfully')
-    //     })
-    //     .catch(error => {
-    //         res.send(error)
-    //         res.sendStatus(400)
-    //     }) 
-})
 
 router.get('/register', (req, res) => {
     User.findAll()
     .then(users => {
+        res.setHeader('content-type', 'application/json');
+        // res.sendStatus(200)
+        res.send(JSON.stringify(users));
         console.log(users)
-        res.sendStatus(200)
     })
     .catch(err => console.log(err))
+})
+
+router.post('/register', (req, res) => {
+    const data = {
+        name: 'maica padillo',
+        email: 'maica_padillo@gmail.com',
+        password: '123456'
+    }
+    // const {
+    //     name,
+    //     email,
+    //     password
+    // } = JSON.stringify(req.body);
+    // res.send(req.body);
+    const { name, email, password} = data;
+    console.log(data);
+    
+    User.create({
+        name,
+        email,
+        password
+    })
+    .then(user => {
+        res.setHeader('content-type', 'application/json')
+        res.send(JSON.stringify(data));
+        console.log(user)
+    })
+    .catch(err => {
+        res.sendStatus(400)
+        console.log(err)
+    });
 })
 
 module.exports = router;
